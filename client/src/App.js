8f3216df8c4c62d20be6faaa3bc7d6429391a6a7
nicolae petre <zeckoinfo@gmail.com>
2024-11-10 19:52:54 +0000

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { AuthProvider } from './providers/AuthProvider';
-import { ErrorBoundary } from './components/error/ErrorBoundary';
 import { ThemeProvider } from './context/ThemeContext';
 import AppRoutes from './routes';
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <ErrorBoundary>
+    <BrowserRouter>
       <ThemeProvider>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <AuthProvider>
-              <AppRoutes />
-            </AuthProvider>
-          </BrowserRouter>
-        </QueryClientProvider>
+        <AppRoutes />
       </ThemeProvider>
-    </ErrorBoundary>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/context/ThemeContext.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import theme from '../styles/theme';
+
+export const ThemeProvider = ({ children }) => {
+  return (
+    <StyledThemeProvider theme={theme}>
+      {children}
+    </StyledThemeProvider>
+  );
+}; 
