60a6febea7b21a7caab3c28d652c71333d0ba910
nicolae petre <zeckoinfo@gmail.com>
2024-12-07 20:55:11 +0000

    fix

--- client/src/styles/GlobalStyles.js
+++ client/src/styles/GlobalStyles.js
@@ -8,8 +8,8 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    background-color: ${({ theme }) => theme.colors.background.default};
-    color: ${({ theme }) => theme.colors.text.primary};
+    background-color: ${({ theme }) => theme?.colors?.background?.default || '#FFFFFF'};
+    color: ${({ theme }) => theme?.colors?.text?.primary || '#333333'};
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 
       'Helvetica Neue', Arial, sans-serif;
   }
