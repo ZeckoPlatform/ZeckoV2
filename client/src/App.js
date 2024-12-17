453f752a541055038196c3128bac7572429fa214
nicolae petre <zeckoinfo@gmail.com>
2024-12-17 21:38:44 +0000

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
-import { ThemeProvider } from './contexts/ThemeContext';
+import { ThemeProvider, useTheme } from './contexts/ThemeContext';
 import CssBaseline from '@mui/material/CssBaseline';
+
+function AppContent() {
+  const { muiTheme } = useTheme();
+  
+  return (
+    <MuiThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          {/* Your routes here */}
+        </Routes>
+      </Router>
+    </MuiThemeProvider>
+  );
+}
 
 function App() {
   return (
     <ThemeProvider>
-      <MuiThemeProvider theme={theme.muiTheme}>
-        <CssBaseline />
-        <Router>
-          <Routes>
-            {/* Your routes here */}
-          </Routes>
-        </Router>
-      </MuiThemeProvider>
+      <AppContent />
     </ThemeProvider>
   );
 }
