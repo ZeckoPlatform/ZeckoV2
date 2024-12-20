be5d5080799275ba56f393ab26b1b2314ff40db9
nicolae petre <zeckoinfo@gmail.com>
2024-12-20 13:10:47 +0000

    change

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import { useAuth } from '../contexts/AuthContext';
 import api from '../services/api';
 
 const Register = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const [formData, setFormData] = useState({
     username: '',
     email: '',
